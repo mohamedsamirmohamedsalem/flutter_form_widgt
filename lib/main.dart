f339427a4565d741310ac6db5e2af9f170a5bd1a
Mohamed Samir <mohamedsamirsalem100@gmail.com>
2020-01-20 18:16:15 +0200

    done

--- lib/main.dart
+++ lib/main.dart
@@ -8,56 +8,77 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String name = 'mohamed';
+  String name = '';
   int phoneNumber = 0114590;
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Form Widget'),
-        ),
-        body: Form(
-          key: formKey,
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Enter Name'),
-                onSaved: (String value) {
-                  name = value;
-                  print('data saved sucessifully');
-                },
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Enter Phone'),
-                onSaved: (String value) {
-                  phoneNumber = int.parse(value);
-                  print('data saved sucessifully');
-                },
-              ),
-              RaisedButton(
-                child: Text('Save Data'),
-                onPressed: () {
-                  setState(() {
-                    formKey.currentState.save();
-                    print('//////////done');
-                  });
-                },
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Text(name),
-              Text(phoneNumber.toString()),
-            ],
+      home: GestureDetector(
+        onTap: () {
+          //this for auto close keyboard when taping in any white space in screen
+          FocusScope.of(context).requestFocus(FocusNode());
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Form Widget'),
+          ),
+          body: Form(
+            key: formKey,
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Enter Name'),
+                  onSaved: (String value) {
+                    name = value;
+                    print('data saved sucessifully');
+                  },
+                  autovalidate: false,
+                  validator: (String value) {
+                    if (value.isEmpty) {
+                      return 'name is required';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                TextFormField(
+                    decoration: InputDecoration(labelText: 'Enter Phone'),
+                    onSaved: (String value) {
+                      phoneNumber = int.parse(value.isEmpty ? "0" : value);
+                      print('data saved sucessifully');
+                    },
+                    validator: (String value) {
+                      if (value.isEmpty) {
+                        return 'phone is required';
+                      }
+                      if (!RegExp(r'^(?:[1-9]\d*|0)(?:\.\d+)?$')
+                          .hasMatch(value)) {
+                        return 'You must enter a number';
+                      }
+                      return null;
+                    }),
+                RaisedButton(
+                  child: Text('Save Data'),
+                  onPressed: () {
+                    setState(() {
+                      formKey.currentState.validate();
+                      formKey.currentState.save();
+                    });
+                  },
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(name),
+                Text(phoneNumber.toString()),
+              ],
+            ),
           ),
         ),
       ),
